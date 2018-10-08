7dec0ca01b18f741409d6f8486805141ac3360a1
SaraTabitha <mayhes06@uwosh.edu>
2018-10-08 09:38:57 -0500

    filling in parking lots
    working to add the rest of the parking lot meta-box functions, much
    like the emergency phones page; trying to add a radio button for the
    accessibility field (maybe should be a checkbox instead); debugging

--- custom-post-types/parking-lots.php
+++ custom-post-types/parking-lots.php
@@ -6,7 +6,7 @@ function create_parking_lots_post_type(){
                 'labels' => array(
                     'name' => __( 'Parking Lots' ),
                     'singular_name' => __( 'Parking Lots'),
-                    'add_new' => __( 'Add Parking Lots' ),
+                    'add_new' => __( 'Add Parking Lot' ),
                     'add_new_item' => __( 'Add New Parking Lot' ),
                     'edit_item' => __( 'Edit Parking Lot' ),
                     'new_item' => __( 'New Parking Lot' ),
@@ -27,16 +27,92 @@ function create_parking_lots_post_type(){
                 ),
                 'public' => true,
                 'has_archive' => true,
-                'menu_icon' => 'dashincons-pressthis',
+                'menu_icon' => 'dashicons-location',
                 'show_in_rest' => true,
                 'register_meta_box_cb' => 'parkings_lots_meta_box_callback',
-                'rewrite' => array('slug' => 'emergency-phones'),
+                'rewrite' => array('slug' => 'parking-lots'),
 
             )
 
         );
 }
 add_action( 'init', 'create_parking_lots_post_type' );
+
+function parking_lots_meta_box_callback(){
+    add_meta_box('parking-lot-info', 'Parking Lot Info', 'parking_lot_info_meta_box', 'parking-lots', 'side');
+
+}
+
+
+function parking_lot_info_meta_box( $post ){
+    // make sure the form request comes from WordPress
+	wp_nonce_field( basename( __FILE__ ), 'location_meta_box_nonce' );
+
+    $latitude = get_post_meta( $post->ID, '_latitude', true );
+    $longitude = get_post_meta( $post->ID, '_longitude', true );
+    $accessible_parking = get_post_meta($post->ID, '_accessibilty', true);
+    ?>
+    
+    Latitude: <input type="number" step="any" name="latitude" value="<?php echo $latitude; ?>" placeholder="Enter a latitude" />
+    Longitude: <input type="number" step="any" name="longitude" value="<?php echo $longitude; ?>" placeholder="Enter a longitude" />
+    Accessible Parking?: <input type="radio" id="accessible_parking" name="accesible_parking" />
+
+    <?php
+}
+
+function parking_lot_info_save_meta_box( $post_id ){
+    // verify taxonomies meta box nonce
+    if ( !isset( $_POST['location_meta_box_nonce'] ) || !wp_verify_nonce( $_POST['info_meta_box_nonce'], basename( __FILE__ ) ) ){
+        return;
+    }
+
+    // return if autosave
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ){
+		return;
+    }
+
+    // Check the user's permissions.
+	if ( ! current_user_can( 'edit_post', $post_id ) ){
+		return;
+    }
+
+    // store location meta box fields
+	// latitude string
+	if ( isset( $_REQUEST['latitude'] ) ) {
+		update_post_meta( $post_id, '_latitude', sanitize_text_field( $_POST['latitude'] ) );
+    }
+    
+    // longitude string
+	if ( isset( $_REQUEST['longitude'] ) ) {
+		update_post_meta( $post_id, '_longitude', sanitize_text_field( $_POST['longitude'] ) );
+    }
+    
+    //accessible parking radiobutton
+    if( isset( $_REQUEST['accessible_parking'])){
+        update_post_meta( $post_id, '_accessibility');
+    }
+}
+// Hooking the function to save data when the update button is clicked
+add_action( 'save_post_parking-lots', 'parking_lot_info_save_meta_box' );
+
+// Configure REST API to include parking lot info data
+function send_parking_lot_info_to_rest(){
+    register_rest_field( 'parking-lots', 'location', array(
+            'get_callback' => 'parse_parking_lot_info_meta_data_for_api'
+        )
+    );
+}
+// Hooking the REST API to include the emergency phone location meta data
+add_action( 'rest_api_init', 'send_parking_lot_info_to_rest' );
+
+// Function that grabs the emergency phones meta data for the REST API
+function parse_parking_lot_info_meta_data_for_api( $post ) {
+    return array(
+        'latitude' => get_post_meta( $post["id"], '_latitude', true ),
+        'longitude' => get_post_meta( $post["id"], '_longitude', true ),
+        'accessible_parking' => get_post_meta( $post["id"], '_accesibility', true)
+    );
+}
 
 
 
